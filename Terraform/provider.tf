d532d4743432083494d83e20d67cf115d3d33c58
reddydevops <33974061+reddydevops@users.noreply.github.com>
2020-05-24 16:54:45 +0530

    asp

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  subscription_id = "${var.subscription_id}"
-  tenant_id       = "${var.tenant_id}"
+  client_id       = "7c5c13b2-44f4-46e8-a927-245199c30d13"
+  client_secret   = "4fk4j3F48_CfeY0bF0~Vb5-zJL0GAyt_Z4"
+  subscription_id = "ab9276f5-ede2-49b7-8620-bd26f4b38ca1"
+  tenant_id       = "1e355c04-e0a4-42ed-8e2d-7351591f0ef1"
    version = "=1.36.0"
 }
